a3ab6bc6b756b760fd2b1439fc3223ed68ccb7ef
Loen15 <59060743+Loen15@users.noreply.github.com>
2022-03-18 15:15:15 +0300

    ssh

--- linters/run.sh
+++ linters/run.sh
@@ -25,9 +25,9 @@ print_header "RUN cppcheck"
 check_log "cppcheck untitled --enable=all --inconclusive --error-exitcode=1 -I untitled/start --suppress=missingIncludeSystem" "\(information\)"
 
 print_header "RUN clang-tidy"
-check_log "clang-tidy untitled/* untitled/start/* -warnings-as-errors=* -extra-arg=-std=c99 -- -Iuntitled/start" "Error (?:reading|while processing)"
+check_log "clang-tidy untitled/start/*.c untitled/start/*.h -warnings-as-errors=* -extra-arg=-std=c99 -- -Istart" "Error (?:reading|while processing)"
 
 print_header "RUN cpplint"
-check_log "cpplint --extensions=c untitled/start/* untitled/*" "Can't open for reading"
+check_log "cpplint --extensions=c untitled/start/*.h untitled/start/*.c untitled/*.c" "Can't open for reading"
 
 print_header "SUCCESS"
