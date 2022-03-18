fc5830771a26a397ba9dd66c09969650fcc3243f
Loen15 <59060743+Loen15@users.noreply.github.com>
2022-03-18 14:26:14 +0300

    ssh

--- untitled/tests/test_stake.cpp
+++ untitled/tests/test_stake.cpp
@@ -20,14 +20,14 @@ TEST(start, incorrect) {
 }
 
 TEST (info_select,correct){
-    stakeholder test={NULL, NULL};
+    /*stakeholder test={NULL, NULL};
     std::string str = "Sponsor";
     test.role = new char[str.length() + 1];
     strcpy(test.role, str.c_str());
     std::string test_file = "../start/test.txt";
     char *correct = new char[test_file.length() + 1];
-    strcpy(correct, test_file.c_str());
-    EXPECT_EQ(test.role,info_select(correct).role);
+    strcpy(correct, test_file.c_str());*/
+    EXPECT_EQ("H",info_select(correct).influence_lvl);
 }
 /*
 
